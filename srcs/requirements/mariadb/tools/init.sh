2bf9412bffb575c49c1d77f37cca90a61b536429
agarcia <agarcia@agarcia.agarcia>
2026-03-19 17:31:53 +0100

    mariadb instalado y configurado dockerfile

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 2
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root shutdown
+
+exec mysqld_safe
